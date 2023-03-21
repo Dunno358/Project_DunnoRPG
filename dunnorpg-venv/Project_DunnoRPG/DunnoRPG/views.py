9553563b76400958080e81a15e826856d61d7623
Mariusz Walczyk <walczyk.mariusz.ipc20@gmail.com>
2023-03-21 21:06:13 +0100

    added static css file

--- dunnorpg-venv/Project_DunnoRPG/DunnoRPG/views.py
+++ dunnorpg-venv/Project_DunnoRPG/DunnoRPG/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Users
 
 def register_view(request):
+    users = Users.objects.all()
     template = loader.get_template('register.html')
-    context = []
+    context = {
+        'users': users,
+    }
     return HttpResponse(template.render())
 
 # Create your views here.
