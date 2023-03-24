240ab65f710b5bfc7a08252eca966814f2c4e375
Mariusz Walczyk <89750169+Dunno358@users.noreply.github.com>
2023-03-24 17:21:27 +0100

    updated master template and added background

--- dunnorpg-venv/Project_DunnoRPG/DunnoRPG/views.py
+++ dunnorpg-venv/Project_DunnoRPG/DunnoRPG/views.py
@@ -3,11 +3,11 @@ from django.template import loader
 from .models import Users
 
 def register_view(request):
-    users = Users.objects.all()
+    users = Users.objects.all().values()
     template = loader.get_template('register.html')
     context = {
         'users': users,
     }
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
