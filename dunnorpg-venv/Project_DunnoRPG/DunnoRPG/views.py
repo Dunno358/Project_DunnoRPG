d3880a2ed0f61d3ab8e0d6e7d15a90d54c125213
Mariusz Walczyk <walczyk.mariusz.ipc20@gmail.com>
2023-03-25 19:09:57 +0100

    added character cards view to templates

--- dunnorpg-venv/Project_DunnoRPG/DunnoRPG/views.py
+++ dunnorpg-venv/Project_DunnoRPG/DunnoRPG/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
+from . import models
 
 def home(request):
-    return render(request, "home.html")
+    context = {
+        'characters': models.Character.objects.all().values()
+    }
+    return render(request, "home.html", context)
 
 class SignUp(CreateView):
     form_class = UserCreationForm
