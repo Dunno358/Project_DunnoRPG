c7b9e215735f4158ffeea6b06f5a1c98e473bec8
Mariusz Walczyk <89750169+Dunno358@users.noreply.github.com>
2023-03-25 04:50:48 +0100

    added login and register, some html and css

--- dunnorpg-venv/Project_DunnoRPG/DunnoRPG/urls.py
+++ dunnorpg-venv/Project_DunnoRPG/DunnoRPG/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from . import views
 #from .views import home,SignUp
 
 urlpatterns = [
-    path('', home, name = "home"),
+    path('', views.home, name = "home"),
+    path("signup/", views.SignUp.as_view(), name="signup"),
 ]
--- dunnorpg-venv/Project_DunnoRPG/Project_DunnoRPG/settings.py
+++ dunnorpg-venv/Project_DunnoRPG/Project_DunnoRPG/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGOUT_REDIRECT_URL = "login"
+LOGOUT_REDIRECT_URL = "home"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
