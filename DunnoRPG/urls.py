0f4330d6ce73c687bec9f0f46b61d37e4144a24f
Mariusz Walczyk <89750169+Dunno358@users.noreply.github.com>
2023-05-29 01:48:22 +0200

    fix for URL to API logging as guest

--- DunnoRPG/urls.py
+++ DunnoRPG/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('character_add_skills/<char_id>/delete/<skill_id>', views.skill_delete, name='skill_delete'),
     path('character_add_skills/<char_id>/upgrade/<skill_id>', views.skill_upgrade, name='skill_upgrade'),
     path('character_add_skills/<char_id>/downgrade/<skill_id>', views.skill_downgrade, name='skill_downgrade'),
+    path('guest/', views.log_as_guest, name='log_as_guest'),
     path('gmpanel/', views.GMPanel.as_view(), name='gm_panel'),
     path('info/', views.Info.as_view(), name='info'),
     path('info/acc-rules/', views.AccRules.as_view(), name='info_acc_rules')
