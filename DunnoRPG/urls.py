7f594ef7e6116ef9240e4cf89d67dba070a63271
Mariusz Walczyk <89750169+Dunno358@users.noreply.github.com>
2023-05-31 04:08:22 +0200

    Feat added stat upgrade/downgrade API URLs

--- DunnoRPG/urls.py
+++ DunnoRPG/urls.py
@@ -6,16 +6,24 @@ from . import views
 urlpatterns = [
     path('', views.charGET.as_view(), name = "home"),
     path("signup/", views.SignUp.as_view(), name="signup"),
+    
     path('character_detail/<id>/', views.CharacterDetails.as_view(), name='character_detail'),
     path('character_add', views.charPOST.as_view(), name="character_add"),
     path('character_add_skills/<id>/', views.CharacterSkills.as_view(), name="character_add_skills"),
+    
     path('skills/', views.Skills.as_view(), name='skills'),
     path('skills/<id>/', views.SkillDetail.as_view(), name='skill_detail'),
+    
     path('character_add_skills/<char_id>/delete/<skill_id>', views.skill_delete, name='skill_delete'),
     path('character_add_skills/<char_id>/upgrade/<skill_id>', views.skill_upgrade, name='skill_upgrade'),
     path('character_add_skills/<char_id>/downgrade/<skill_id>', views.skill_downgrade, name='skill_downgrade'),
+    
+    path('character_add_skills/<int:char_id>/up/<str:stat>', views.UpgradeCharacterStats.as_view(), name='stat_upgrade'),
+    path('character_add_skills/<int:char_id>/down/<str:stat>', views.DowngradeCharacterStats.as_view(), name='stat_downgrade'),
+    
     path('guest/', views.log_as_guest, name='log_as_guest'),
     path('gmpanel/', views.GMPanel.as_view(), name='gm_panel'),
+    
     path('info/', views.Info.as_view(), name='info'),
     path('info/acc-rules/', views.AccRules.as_view(), name='info_acc_rules')
 ]
