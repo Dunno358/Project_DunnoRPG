76cf15d431aee18f65641c689650ff5b16ca4ad3
Mariusz Walczyk <89750169+Dunno358@users.noreply.github.com>
2023-06-01 21:05:43 +0200

    Feat 404 view

--- DunnoRPG/views.py
+++ DunnoRPG/views.py
@@ -508,3 +508,6 @@ class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+    
+def view_404(request, exception):
+    return render(request, '404.html', status=404)
