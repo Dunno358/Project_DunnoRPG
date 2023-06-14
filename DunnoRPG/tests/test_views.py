27de528314c5073f1ddd4339fca699018d51a855
Mariusz Walczyk <89750169+Dunno358@users.noreply.github.com>
2023-06-14 17:33:17 +0200

    Test SkillDowngrade API

--- DunnoRPG/tests/test_views.py
+++ DunnoRPG/tests/test_views.py
@@ -241,4 +241,145 @@ class SkillUpgradeTest(TestCase):
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), f'Your stats are too low to upgrade {self.normal_skill.skill}, you need SIŁ(3)') 
         
-#Skill downgrade API to be tested next
+    def test_invalid_character_id(self):
+        self.client.login(username='testuser', password='testpass') 
+        response = self.client.post(reverse('skill_upgrade', args=[100, self.normal_skill.id]))
+        self.assertEqual(response.status_code, 404)
+        
+    def test_invalid_skill_id(self):
+        self.client.login(username='testuser', password='testpass') 
+        response = self.client.post(reverse('skill_upgrade', args=[self.character.id, 100]))
+        self.assertEqual(response.status_code, 404)
+        
+class SkillDowngradeTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.magical_skill = models.Skills.objects.create(
+            id=1,
+            owner = 'testowner1',
+            character = 'testcharacter1',
+            skill = 'Kula ognia',
+            category = 'Magical',
+            level = 2,
+            desc = 'testdesc'
+        )
+        
+        self.magical_skill_desc = models.Skills_Decs.objects.create(
+            id=1,
+            name = 'Kula ognia',
+            category = 'Magical',
+            desc = 'TestDesc',
+            level1 = 'l1desc',
+            level2 = 'l2desc',
+            level3 = 'l3desc',
+            level4 = 'l4desc',
+            cost = '1',
+            useAmount = 2
+        )
+        
+        self.normal_skill = models.Skills.objects.create(
+            id=2,
+            owner = 'testowner1',
+            character = 'testcharacter1',
+            skill = 'Cios berserkera',
+            category = 'Melee',
+            level = 2,
+            desc = 'testdesc'
+        )
+        
+        self.normal_skill_desc = models.Skills_Decs.objects.create(
+            id=2,
+            name = 'Cios berserkera',
+            category = 'Melee',
+            desc = 'TestDesc',
+            level1 = 'l1desc',
+            need1_1= 'SIŁ2',
+            level2 = 'l2desc',
+            need2_1= 'SIŁ3',
+            level3 = 'l3desc',
+            need3_1= 'SIŁ4',
+            cost = '1',
+            useAmount = 1
+        )
+        
+        self.character = models.Character.objects.create(
+            id=1, 
+            owner = 'testuser',
+            name = f'testcharacter1',
+            race = 'Człowiek(Imperium)',
+            size = 'M',
+            HP= 25,
+            fullHP = 25,
+            INT = 3,
+            SIŁ = 5,
+            ZRE = 1,
+            CHAR = 1,
+            CEL = 1,
+            points_left=4)
+        
+    def test_skill_dowgrade_success(self):
+        self.client.login(username='testuser', password='testpass')
+        
+        #Magical skill
+        url = reverse('skill_downgrade', args=[self.character.id, self.magical_skill.id])
+        response = self.client.post(url)
+        
+        self.assertEqual(response.status_code, 302) #302 for redirect
+        
+        updated_skill = models.Skills.objects.get(id=self.magical_skill.id)
+        self.assertEqual(updated_skill.level, 1)
+        self.assertEqual(updated_skill.desc, f"{self.magical_skill_desc.desc} {self.magical_skill_desc.level1}")
+        
+        updated_character = models.Character.objects.get(id=self.character.id)
+        self.assertEqual(updated_character.points_left, self.character.points_left) #shouldn't change
+        
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(messages, []) #no error messages
+        
+        #Melee skill
+        url = reverse('skill_downgrade', args=[self.character.id, self.normal_skill.id])
+        response = self.client.post(url)   
+        
+        self.assertEqual(response.status_code, 302)
+             
+        updated_skill = models.Skills.objects.get(id=self.normal_skill.id)
+        self.assertEqual(updated_skill.level, 1)
+        self.assertEqual(updated_skill.desc, f"{self.normal_skill_desc.desc} {self.normal_skill_desc.level1}")
+        
+        updated_character = models.Character.objects.get(id=self.character.id)
+        self.assertEqual(updated_character.points_left, self.character.points_left+1)
+        
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(messages, []) #no error messages
+        
+    def test_minimal_level(self):
+        self.magical_skill.level = 1
+        self.magical_skill.save()
+        self.normal_skill.level = 1
+        self.normal_skill.save()
+        
+        #Magical skill
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.post(reverse('skill_downgrade', args=[self.character.id, self.magical_skill.id]))  
+        self.assertEqual(response.status_code, 302)    
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), f"{self.magical_skill.skill} level cannot be lower!")
+        
+        #Normal skill
+        response2 = self.client.post(reverse('skill_downgrade', args=[self.character.id, self.normal_skill.id]))
+        self.assertEqual(response2.status_code, 302)
+        messages = list(get_messages(response2.wsgi_request))
+        self.assertEqual(len(messages), 2)
+        self.assertEqual(str(messages[1]), f"{self.normal_skill.skill} level cannot be lower!")
+        
+    def test_invalid_character_id(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.post(reverse('skill_downgrade', args=[100, self.magical_skill.id])) 
+        self.assertEqual(response.status_code, 404)
+        
+    def test_invalid_skill_id(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.post(reverse('skill_downgrade', args=[self.character.id, 100])) 
+        self.assertEqual(response.status_code, 404)
+        
